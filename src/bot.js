24192d5a179a2719bd1c3d573579e461fba3bfe9
Teo <90730116+C-Teo@users.noreply.github.com>
2025-02-01 12:53:31 -0500

    feat: create a sample command and begin work on registering it

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,8 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv'
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js'
 
 dotenv.config()
 
@@ -9,4 +12,14 @@ const client = new Client({
     ],
 });
 
-client.login(process.env.DISCORD_TOKEN)
+client.commmands = new Collection();
+
+// Getting the commands from our commands/ folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+console.log(commandFolders)
+
+// client.login(process.env.DISCORD_TOKEN)
