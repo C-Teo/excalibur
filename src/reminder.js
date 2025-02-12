67696badffc85a6ee44cc9ed6fa76defe5faf732
Teo <90730116+C-Teo@users.noreply.github.com>
2025-02-11 23:56:13 -0500

    feat: reminder class; will have to rename other file

---
+++ src/reminder.js
@@ -0,0 +1,33 @@
+class Reminder {
+	constructor(
+		task,
+		taskDescription,
+		taskDueDate,
+		reminderType,
+		assignee,
+		done
+	) {
+		this.task = task;
+		this.taskDescription = taskDescription;
+		this.taskDueDate = taskDueDate;
+		this.reminderType = reminderType;
+		this.assignee = assignee;
+		this.done = done;
+	}
+
+	getFormattedDueDate() {
+		return this.taskDueDate.toLocaleDateString();
+	}
+
+	isDone() {
+		return this.done;
+	}
+
+	toString() {
+		return `Task: ${this.task}, Description: ${
+			this.taskDescription
+		}, Due Date: ${this.getFormattedDueDate()}, Reminder Type: ${
+			this.reminderType
+		}, Assignee: ${this.assignee}, Done: ${this.done}`;
+	}
+}
